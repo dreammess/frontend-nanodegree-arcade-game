4e9a9ff127f496535bdaedfad671aedda676e7ee
Magdalena <magdalena.r.stevens@gmail.com>
2017-07-03 10:35:57 +0200

    Added characters and enemies
    Characters and enemey sprites were added and wired up for movement and
    collision detection. Character resets to initial position if she
    encounters a wall or an enemy.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.x = 0;
+    this.y = y;
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -14,6 +17,15 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + this.speed * dt;
+    if (this.x >= 515) {
+        this.x = -75;
+    }
+
+    // Collision
+    if(this.x < player.x + 30 && this.x + 60 > player.x && this.y < player.y + 60 && this.y + 40 > player.y) {
+        player.reset();
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,11 +36,65 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.x = 217;
+    this.y = 430;
+    this.speed = 15;
+    this.sprite = 'images/char-cat-girl.png';
+};
 
+Player.prototype.update = function(dt) {
+
+    // reaches water = reset:
+    if(this.y < 0 || this.y > 450 || this.x < -25 || this.x > 450) {
+        this.reset();
+    }
+
+    allEnemies.forEach(function(enemy) {
+    if(self.x >= enemy.x - 25 && self.x <= enemy.x + 25) {
+        if(self.y >= enemy.y - 25 && self.y <= enemy.y + 25) {
+            self.reset();
+            }
+        }
+    });
+};
+
+//Reset player to beginning position
+Player.prototype.reset = function() {
+    'use strict';
+   this.x = 200;
+   this.y = 400;
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(direction) {
+    if (direction === 'left') {
+        this.x -= this.speed;
+    } else if(direction === 'right') {
+        this.x += this.speed;
+    } else if(direction === 'up') {
+        this.y -= this.speed;
+    } else if(direction === 'down') {
+        this.y += this.speed;
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var player = new Player();
+
+var allEnemies = [];
+var enemy1 = new Enemy(102, 120);
+var enemy2 = new Enemy(202, 150);
+var enemy3 = new Enemy(303, 170);
+allEnemies.push(enemy1);
+allEnemies.push(enemy2);
+allEnemies.push(enemy3);
+
 
 
 
